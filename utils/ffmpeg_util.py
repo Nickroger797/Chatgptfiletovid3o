0168ada5cd168208a0d3e366fcc9713986914061
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-30 20:10:42 +0530

    Delete utils directory

--- utils/ffmpeg_util.py
+++
@@ -1,8 +0,0 @@
-import ffmpeg
-import os
-
-def convert_video(input_path):
-    output_path = input_path.rsplit(".", 1)[0] + "_converted.mp4"
-    
-    ffmpeg.input(input_path).output(output_path, vcodec='libx264', preset='ultrafast').run()
-    return output_path
