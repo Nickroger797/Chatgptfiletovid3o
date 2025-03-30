9da63274ebc747c5904299f347fa2ed0c2ab98ae
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-30 20:57:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Update and Install FFmpeg
+apt update && apt install -y ffmpeg
+
 # Ensure all necessary permissions
 chmod +x start.sh
 
 # Activate virtual environment (अगर कोई हो)
 # source venv/bin/activate  
 
+# Start the bot
 python3 bot.py &  # Bot बैकग्राउंड में रन होगा
 python3 healthcheck.py  # Health check सर्वर रन करेगा
