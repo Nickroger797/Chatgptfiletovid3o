d34ae2bf652fda6da9f8e5d1836290a31a050e40
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-30 14:08:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,9 @@
 from flask import Flask
-import threading
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!", 200
+    return "Bot is running!"
 
-def run_server():
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
-def start_flask():
-    thread = threading.Thread(target=run_server)
-    thread.daemon = True
-    thread.start()
