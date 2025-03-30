b7570c1cde4ebad45ecb57e5be2f59e984570451
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-30 18:17:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 chmod +x healthcheck.py
+chmod +x start.sh
+git add start.sh
+git commit -m "Make start.sh executable"
+git push origin main  # या जो भी branch हो
 python3 bot.py &  # Bot बैकग्राउंड में रन होगा
 python3 healthcheck.py  # Health check सर्वर रन करेगा
