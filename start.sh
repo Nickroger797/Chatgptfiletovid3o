2cd8096651172369d346de045f4936a56962a215
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-30 21:50:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 
 echo "✅ Starting Video Converter Bot..."
 
-# स्टार्ट.sh को एक्सीक्यूटेबल बनाओ (सिर्फ एक बार, अगर जरूरी हो)
-chmod +x start.sh
+# Flask Health Check Server बैकग्राउंड में चलाओ
+python3 healthcheck.py &  
 
-# बॉट स्टार्ट करो
+# Telegram Bot स्टार्ट करो
 python3 bot.py
-python3 healthcheck.py  # Health check सर्वर रन करेगा
