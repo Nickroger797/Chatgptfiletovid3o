dd4fd03899772c2028624181029aa9416ba5adca
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-30 12:30:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))  # Replace with your API_ID
+API_HASH = os.getenv("API_HASH", "your_api_hash")  # Replace with your API_HASH
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")  # Replace with your BOT_TOKEN
+MONGO_URL = os.getenv("MONGO_URL", "your_mongo_url")  # Replace with your MongoDB URL
